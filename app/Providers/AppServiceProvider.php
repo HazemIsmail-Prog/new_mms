4ede9296d0af663035fc684052cf5cd65f2e499e
HazemIsmail-Prog <67516349+HazemIsmail-Prog@users.noreply.github.com>
2023-07-15 11:22:13 +0300

    add is production to appserviceprovider lazy loading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        Model::preventLazyLoading();
+        Model::preventLazyLoading(!app()->isProduction());
+
     }
 }
