b84d41d02020652eb9440f3bb22ea5ee6aa778d2
HazemIsmail-Prog <67516349+HazemIsmail-Prog@users.noreply.github.com>
2023-11-15 18:12:45 +0300

    hide customer phone and address untill click accept

--- resources/views/livewire/technician-page.blade.php
+++ resources/views/livewire/technician-page.blade.php
@@ -12,18 +12,18 @@
                         <th>{{ __('messages.customer_name') }}</th>
                         <td>{{ $order->customer->name }}</td>
                     </tr>
-                    <tr>
-                        <th>{{ __('messages.phone') }}</th>
-                        <td>
-                            <span>{{ $order->phone->number }}</span>
-                            <a class="text-dark btn btn-sm" target="_blank"
-                                href="https://wa.me/+965{{ $order->phone->number }}">
-                                <svg fill="#000000" height="20px" width="20px" version="1.1" id="Layer_1"
-                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                    viewBox="0 0 308 308" xml:space="preserve">
-                                    <g id="XMLID_468_">
-                                        <path id="XMLID_469_"
-                                            d="M227.904,176.981c-0.6-0.288-23.054-11.345-27.044-12.781c-1.629-0.585-3.374-1.156-5.23-1.156
+                    @if ($order->status_id != 2)
+                        <tr>
+                            <th>{{ __('messages.phone') }}</th>
+                            <td>
+                                <span>{{ $order->phone->number }}</span>
+                                <a class="text-dark btn btn-sm" target="_blank"
+                                    href="https://wa.me/+965{{ $order->phone->number }}">
+                                    <svg fill="#000000" height="20px" width="20px" version="1.1" id="Layer_1"
+                                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        viewBox="0 0 308 308" xml:space="preserve">
+                                        <g id="XMLID_468_">
+                                            <path id="XMLID_469_" d="M227.904,176.981c-0.6-0.288-23.054-11.345-27.044-12.781c-1.629-0.585-3.374-1.156-5.23-1.156
                                             c-3.032,0-5.579,1.511-7.563,4.479c-2.243,3.334-9.033,11.271-11.131,13.642c-0.274,0.313-0.648,0.687-0.872,0.687
                                             c-0.201,0-3.676-1.431-4.728-1.888c-24.087-10.463-42.37-35.624-44.877-39.867c-0.358-0.61-0.373-0.887-0.376-0.887
                                             c0.088-0.323,0.898-1.135,1.316-1.554c1.223-1.21,2.548-2.805,3.83-4.348c0.607-0.731,1.215-1.463,1.812-2.153
@@ -33,33 +33,34 @@
                                             c17.873,26.102,40.154,45.446,62.741,54.469c21.745,8.686,32.042,9.69,37.896,9.69c0.001,0,0.001,0,0.001,0
                                             c2.46,0,4.429-0.193,6.166-0.364l1.102-0.105c7.512-0.666,24.02-9.22,27.775-19.655c2.958-8.219,3.738-17.199,1.77-20.458
                                             C233.168,179.508,230.845,178.393,227.904,176.981z" />
-                                        <path id="XMLID_470_"
-                                            d="M156.734,0C73.318,0,5.454,67.354,5.454,150.143c0,26.777,7.166,52.988,20.741,75.928L0.212,302.716
+                                            <path id="XMLID_470_" d="M156.734,0C73.318,0,5.454,67.354,5.454,150.143c0,26.777,7.166,52.988,20.741,75.928L0.212,302.716
                                             c-0.484,1.429-0.124,3.009,0.933,4.085C1.908,307.58,2.943,308,4,308c0.405,0,0.813-0.061,1.211-0.188l79.92-25.396
                                             c21.87,11.685,46.588,17.853,71.604,17.853C240.143,300.27,308,232.923,308,150.143C308,67.354,240.143,0,156.734,0z
                                             M156.734,268.994c-23.539,0-46.338-6.797-65.936-19.657c-0.659-0.433-1.424-0.655-2.194-0.655c-0.407,0-0.815,0.062-1.212,0.188
                                             l-40.035,12.726l12.924-38.129c0.418-1.234,0.209-2.595-0.561-3.647c-14.924-20.392-22.813-44.485-22.813-69.677
                                             c0-65.543,53.754-118.867,119.826-118.867c66.064,0,119.812,53.324,119.812,118.867
                                             C276.546,215.678,222.799,268.994,156.734,268.994z" />
-                                    </g>
-                                </svg>
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>{{ __('messages.address') }}</th>
-                        <td>
-                            <span>{{ $order->address->full_address() }}</span>
-                            <a class="text-dark btn btn-sm" target="_blank"
-                                href="{{ $order->address->maps_search() }}">
-                                <svg style="width: 15px;height: 15px">
-                                    <use
-                                        xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-location-pin') }}">
-                                    </use>
-                                </svg>
-                            </a>
-                        </td>
-                    </tr>
+                                        </g>
+                                    </svg>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>{{ __('messages.address') }}</th>
+                            <td>
+                                <span>{{ $order->address->full_address() }}</span>
+                                <a class="text-dark btn btn-sm" target="_blank"
+                                    href="{{ $order->address->maps_search() }}">
+                                    <svg style="width: 15px;height: 15px">
+                                        <use
+                                            xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-location-pin') }}">
+                                        </use>
+                                    </svg>
+                                </a>
+                            </td>
+                        </tr>
+                    @endif
+
                     @if ($order->order_description)
                         <tr>
                             <th>@lang('messages.order_description')</th>
@@ -87,12 +88,10 @@
                                 @break
 
                                 @case(7)
-                                    <button 
+                                    <button
                                         {{ $order->invoices->count() == 0 && config('global.invoice_required') ? 'disabled' : '' }}
-                                        wire:loading.attr="disabled"
-                                        onclick="confirmComplete()" 
-                                        class="btn btn-success"
-                                    >@lang('messages.done')</button>
+                                        wire:loading.attr="disabled" onclick="confirmComplete()"
+                                        class="btn btn-success">@lang('messages.done')</button>
                                 @break
                             @endswitch
                         </td>
